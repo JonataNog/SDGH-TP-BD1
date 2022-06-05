0f511acae5bf768c2ea7dddaf0120a913c0fd309
Jonata Nogueira <jonatalucas2002@gmail.com>
2022-06-05 11:30:02 -0300

    Validation exception

--- src/gui/ClinicFormController.java
+++ src/gui/ClinicFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Clinic;
+import model.exceptions.ValidationException;
 import model.services.ClinicService;
 
 public class ClinicFormController implements Initializable{
@@ -66,6 +69,9 @@ public class ClinicFormController implements Initializable{
 			notifyDataChangeListeners();
 			Utils.currentStage(event).close();
 		}
+		catch(ValidationException e ) {
+			setErrorMessages(e.getErrors());
+		}
 		catch(DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
 		}
@@ -79,9 +85,25 @@ public class ClinicFormController implements Initializable{
 
 	private Clinic getFormData() {
 		Clinic obj = new Clinic();
+		
+		ValidationException exception = new ValidationException("Validation error");
+		
+		if(txtCnpj.getText() == null || txtCnpj.getText().trim().equals("")) {
+			exception.addError("cnpj", "Field can't be empty");
+		}
 		obj.setCnpj(txtCnpj.getText());
+		if(txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "Field can't be empty");
+		}
 		obj.setName(txtName.getText());
+		if(txtLocal.getText() == null || txtLocal.getText().trim().equals("")) {
+			exception.addError("local", "Field can't be empty");
+		}
 		obj.setLocal(txtLocal.getText());
+		
+		if(exception.getErrors().size() > 0) {
+			throw exception;
+		}
 		
 		return obj;
 	}
@@ -123,4 +145,17 @@ public class ClinicFormController implements Initializable{
 		txtLocal.setText(entity.getLocal());
 	}
 
+	public void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if(fields.contains("cnpj")) {
+			labelErrorCnpj.setText(errors.get("cnpj"));
+		}
+		if(fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
+		if(fields.contains("local")) {
+			labelErrorLocal.setText(errors.get("local"));
+		}
+	}
 }
